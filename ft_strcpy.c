72bd2199fc4494f1bbe95baab669c8a57307699a
honeybadgerdad <67338476+honeybadgerdad@users.noreply.github.com>
2020-07-03 21:04:07 +0200

    strdup and strcpy updated

--- ft_strcpy.c
+++ ft_strcpy.c
@@ -1,18 +1,18 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 /*int ft_strlen(char *str1);*/
 
-char ft_strcpy(char *dest, char *src)
+char *ft_strcpy(char *dest,char *src)
 {
     int i;
     i = 0;
-   /* dest = (char *) malloc(ft_strlen(src));*/
-    while (src[i] != '/0')
+    while (src[i] != '\0')
     {
         dest[i] = src[i];
         i++;
     }
-    dest[i] = '/0';
+    dest[i] = '\0';
     return (dest);
 }
--- ft_strdup.c
+++ ft_strdup.c
@@ -2,10 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 int ft_strlen(char *str1);
-char ft_strcpy(char *dest, char *src);
-char ft_strdup(char *str)
+char *ft_strcpy(char *dest, char *src);
+char *ft_strdup(char *str)
 {
-   char *str2 = (char*)malloc((ft_strlen(str)+1) * sizeof(char*));
+    char *str2;
+   str2 = (char*)malloc(ft_strlen(str)+1 * sizeof(str2));  
     ft_strcpy(str2, str);
     return(str2);
 }
@@ -13,11 +14,11 @@ char ft_strdup(char *str)
 int main() 
 { 
     char str[] = "GeeksForGeeks"; 
-  
+   
     // A copy of source is created dynamically 
     // and pointer to copy is returned. 
     char* str2 = ft_strdup(str);  
   
-    printf("%s", str2); 
+   printf("%s", str2); 
     return 0; 
 } 
